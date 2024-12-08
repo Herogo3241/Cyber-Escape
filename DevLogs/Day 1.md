ec5b1aa0bb5984f67bbbefa71ea1ce4dbac3cad6
herogo3241 <goureesh.chandra23@gmail.com>
2024-12-08 08:23:33 +0530

    minor change

--- DevLogs/Day 1.md
+++ DevLogs/Day 1.md
@@ -5,6 +5,9 @@
 1. Setup Raylib with c++
 2.  Created Window
 3.  Added a test player character
+4. Added Border collision
+
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,61 +1,144 @@
 #include "raylib.h"
 
+struct Box
+{
+    int x, y;
+    Color color;
+
+    void draw()
+    {
+        DrawRectangle(x, y, 30, 30, color);
+    }
+};
+
 struct Player
 {
-  int x, y;
-  int speed;
-  Color color;
-  void move()
-  {
-    if (IsKeyDown(KEY_D))
-    {
-      x += speed;
-    }
-    if (IsKeyDown(KEY_A))
-    {
-      x -= speed;
-    }
-    if (IsKeyDown(KEY_W))
-    {
-      y -= speed;
-    }
-    if (IsKeyDown(KEY_S))
-    {
-      y += speed;
-    }
-  }
-  void draw()
-  {
-    DrawRectangle(x, y, 50, 50, color);
-  }
+    int x, y;
+    int speed;
+    Color color;
 
+    void move(Box boxes[], int numBoxes)
+    {
+        // Create a copy of the player's current position
+        int newX = x;
+        int newY = y;
+
+        // Update position based on key press
+        if (IsKeyDown(KEY_D))
+        {
+            newX += speed;
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            newX -= speed;
+        }
+        if (IsKeyDown(KEY_W))
+        {
+            newY -= speed;
+        }
+        if (IsKeyDown(KEY_S))
+        {
+            newY += speed;
+        }
+
+        // Check collision with all boxes
+        Rectangle newPlayerRect = {(float)newX, (float)newY, 30, 30};
+        bool collision = false;
+
+        for (int i = 0; i < numBoxes; i++)
+        {
+            Rectangle boxRect = {(float)boxes[i].x, (float)boxes[i].y, 30, 30};
+            if (CheckCollisionRecs(newPlayerRect, boxRect))
+            {
+                collision = true;
+                break;
+            }
+        }
+
+        // If no collision, update the player's position
+        if (!collision)
+        {
+            x = newX;
+            y = newY;
+        }
+
+        // Ensure the player stays within screen boundaries
+        if (x < 0 || x > GetScreenWidth() - 30 || y < 0 || y > GetScreenHeight() - 30)
+        {
+            x = x < 0 ? 0 : x > GetScreenWidth() - 30 ? GetScreenWidth() - 30
+                                                      : x;
+            y = y < 0 ? 0 : y > GetScreenHeight() - 30 ? GetScreenHeight() - 30
+                                                       : y;
+        }
+    }
+
+    void draw()
+    {
+        DrawRectangle(x, y, 30, 30, color);
+    }
 };
+
+const char *text = "Press WASD to move the square";
 
 int main()
 {
-  const int screenWidth = 1080;
-  const int screenHeight = 960;
-  InitWindow(screenWidth, screenHeight, "First Game");
-  SetTargetFPS(60);
+    int display = GetCurrentMonitor();
+    const int screenWidth = 800;
+    const int screenHeight = 600;
 
-  Player player;
-  player.x = 400;
-  player.y = 400;
-  player.speed = 5;
-  player.color = RED;
+    InitWindow(screenWidth, screenHeight, "First Game");
 
-  while (!WindowShouldClose())
-  {
+    SetTargetFPS(60);
 
-    player.move();
+    Player player;
+    player.x = screenWidth / 2 - 25;
+    player.y = screenHeight / 2 - 25;
+    player.speed = 5;
+    player.color = RED;
 
+    const int numBoxes = 10;
+    Box boxes[numBoxes];
 
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    // DrawText("It works!", 20, 20, 20, BLACK);
-    player.draw();
-    EndDrawing();
-  }
-  CloseWindow();
-  return 0;
+    // Randomly place boxes
+    for (int i = 0; i < numBoxes; i++)
+    {
+        boxes[i].x = GetRandomValue(0, screenWidth - 50);
+        boxes[i].y = GetRandomValue(0, screenHeight - 50);
+        boxes[i].color = (Color){(unsigned char)(GetRandomValue(20, 255)), (unsigned char)(GetRandomValue(20, 255)), (unsigned char)(GetRandomValue(20, 255)), 255};
+    }
+
+    while (!WindowShouldClose())
+    {
+        player.move(boxes, numBoxes);
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText(text, screenWidth / 2 - MeasureText(text, 20) / 2, 20, 20, BLACK);
+
+        for (int i = 0; i < numBoxes; i++)
+        {
+            boxes[i].draw();
+        }
+
+        if (IsKeyDown(KEY_Q))
+        {
+            CloseWindow();
+            return 0;
+        }
+        if (IsKeyDown(KEY_R))
+        {
+            for (int i = 0; i < numBoxes; i++)
+            {
+                boxes[i].x = GetRandomValue(0, screenWidth - 50);
+                boxes[i].y = GetRandomValue(0, screenHeight - 50);
+                boxes[i].color = (Color){(unsigned char)(GetRandomValue(20, 255)), (unsigned char)(GetRandomValue(20, 255)), (unsigned char)(GetRandomValue(20, 255)), 255};
+            }
+        }
+
+        player.draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
